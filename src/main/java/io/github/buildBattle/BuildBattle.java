2ab61b84c126fe46c9d38ab801b226b7781c2682
faustyu1 <faustyu@vk.com>
2025-04-30 15:55:52 +0400

    initial commit

---
+++ src/main/java/io/github/buildBattle/BuildBattle.java
@@ -0,0 +1,17 @@
+package io.github.buildBattle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BuildBattle extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
